de40e83bab4cd9c8d71acbf5bb5c0911c8a1e1b3
Mauricio Arce <114115194+mauritk@users.noreply.github.com>
2023-01-25 18:00:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById('menu');
+
+let toggle_open = document.getElementById('toggle_open')
+let toggle_close = document.getElementById('toggle_close')
+
+toggle_open.addEventListener('click', toggleMenu)
+toggle_close.addEventListener('click', toggleMenu)
+
+function toggleMenu(){
+    menu.classList.toggle('show-menu')
+}
